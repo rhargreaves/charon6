0cef79ba74cc5de5f87ce62fc14e1cc82048ea5d
Robert Hargreaves <kiyouta@gmail.com>
2026-06-28 14:47:19 +0000

    refactor: trim lib.rs to only export what main.rs uses

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,5 @@ mod packet;
 mod sender;
 
 pub use capture::{capture_loop, open_ipv6_packet_socket};
-pub use cidr::{Ipv6Cidr, ParseCidrError};
-pub use codec::{DecodeError, Frame, decode_dst, encode_dst};
-pub use packet::parse_ipv6_packet;
+pub use cidr::Ipv6Cidr;
 pub use sender::send_message;

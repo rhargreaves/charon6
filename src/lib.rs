867de83a6280526b6cd4b0f34252d1ce56cdd1f5
Robert Hargreaves <kiyouta@gmail.com>
2026-06-29 19:44:14 +0000

    refactor: rename capture.rs to receiver.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
-mod capture;
 mod cidr;
 mod cipher;
 mod codec;
 mod packet;
+mod receiver;
 mod sender;
 #[cfg(test)]
 mod test_helpers;
 
-pub use capture::{capture_loop, open_ipv6_packet_socket};
 pub use cidr::Ipv6Cidr;
 pub use cipher::Cipher;
+pub use receiver::{capture_loop, open_ipv6_packet_socket};
 pub use sender::send_message;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
